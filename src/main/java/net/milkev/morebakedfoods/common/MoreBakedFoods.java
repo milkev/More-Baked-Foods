63a5bd4fda3d677cf0f37cb8db2bf0f7ab96e579
milkev <kmdre@hotmail.com>
2021-03-21 17:21:13 -0600

    Base Commit
    base mod with no actual changes to the game or any code written

---
+++ src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
@@ -0,0 +1,9 @@
+package net.milkev.morebakedfoods.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreBakedFoods implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
