677da9ebdaf36f84a878461c717847f5ef4cd350
milkev <kmdre@hotmail.com>
2021-03-21 19:54:03 -0600

    Added Roasted beetroot/pumpkin
    roasted beetroot: 4/3
    roasted pumpkin: 6/2
    baked carrot tweaked to: 5/4

--- src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
+++ src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
@@ -14,14 +14,22 @@ public class MoreBakedFoods implements ModInitializer {
 	public static final String MOD_ID = "morebakedfoods";
 	public static final String MOD_NAME = "More Baked Foods";
 
-	public static final FoodComponent.Builder FOOD_H_5_0_S_1_0_BUILDER = new FoodComponent.Builder().hunger(4).saturationModifier(1.0f);
-	public static final FoodComponent FOOD_H_5_0_S_1_0 = FOOD_H_5_0_S_1_0_BUILDER.build();
+	public static final FoodComponent.Builder FOOD_H_5_0_S_4_0_BUILDER = new FoodComponent.Builder().hunger(5).saturationModifier(0.5f);
+	public static final FoodComponent FOOD_H_5_0_S_4_0 = FOOD_H_5_0_S_4_0_BUILDER.build();
+	public static final FoodComponent.Builder FOOD_H_4_0_S_3_0_BUILDER = new FoodComponent.Builder().hunger(4).saturationModifier(0.375f);
+	public static final FoodComponent FOOD_H_4_0_S_3_0 = FOOD_H_4_0_S_3_0_BUILDER.build();
+	public static final FoodComponent.Builder FOOD_H_6_0_S_2_0_BUILDER = new FoodComponent.Builder().hunger(6).saturationModifier(0.25f);
+	public static final FoodComponent FOOD_6_0_S_2_0 = FOOD_H_6_0_S_2_0_BUILDER.build();
 
-	public static final Item BAKED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_1_0).group(ItemGroup.MISC));
+	public static final Item BAKED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_4_0).group(ItemGroup.MISC));
+	public static final Item ROASTED_BEETROOT = new Item(new FabricItemSettings().food(FOOD_H_4_0_S_3_0).group(ItemGroup.MISC));
+	public static final Item ROASTED_PUMPKIN = new Item(new FabricItemSettings().food(FOOD_6_0_S_2_0).group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "baked_carrot"), BAKED_CARROT);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_beetroot"), ROASTED_BEETROOT);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_pumpkin"), ROASTED_PUMPKIN);
 
 		System.out.println("More Baked Foods Initialized");
 	}
