b8f58e27f492c2cd58033d1e5abee2d93e98fe4b
milkev <kmdre@hotmail.com>
2021-03-21 18:36:24 -0600

    Added Baked Carrot
    added Baked Carrot, Currently restores 4 hunger and 1 saturation. Waiting for texture

--- src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
+++ src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
@@ -1,9 +1,31 @@
 package net.milkev.morebakedfoods.common;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class MoreBakedFoods implements ModInitializer {
+
+
+	public static final String MOD_ID = "morebakedfoods";
+	public static final String MOD_NAME = "More Baked Foods";
+
+	public static final FoodComponent.Builder FOOD_H_5_0_S_1_0_BUILDER = new FoodComponent.Builder().hunger(4).saturationModifier(1.0f);
+	public static final FoodComponent FOOD_H_5_0_S_1_0 = FOOD_H_5_0_S_1_0_BUILDER.build();
+
+	public static final Item BAKED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_1_0).group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "baked_carrot"), BAKED_CARROT);
+
+		System.out.println("More Baked Foods Initialized");
 	}
+
+
+
 }
