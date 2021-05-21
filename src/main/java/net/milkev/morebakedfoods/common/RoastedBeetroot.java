8d55dad7bc245a7ba948319dd52ece1201c31cfb
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-05-21 15:28:08 -0400

    Ported Roasted Beetroot
    to forge 1.16.5

---
+++ src/main/java/net/milkev/morebakedfoods/common/RoastedBeetroot.java
@@ -0,0 +1,20 @@
+package net.milkev.morebakedfoods.common;
+
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+
+public class RoastedBeetroot extends Item {
+    public RoastedBeetroot() {
+        super(new Properties()
+
+                .group(ItemGroup.MISC)
+                .food(new Food.Builder()
+                        .hunger(4)
+                        .saturation(0.375F)
+                        .build()
+                ));
+    }
+
+}
