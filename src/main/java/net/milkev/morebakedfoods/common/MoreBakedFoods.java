d6774b2b97d28e45d2b3cf0bba3a458584501712
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-05-21 15:02:52 -0400

    Set up workspace for  forge
    1.16.5

--- src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
+++ src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
@@ -1,53 +1,37 @@
 package net.milkev.morebakedfoods.common;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-public class MoreBakedFoods implements ModInitializer {
-
-
+@Mod("morebakedfoods")
+public class MoreBakedFoods {
 	public static final String MOD_ID = "morebakedfoods";
 
-	//roasted carrot
-	public static final FoodComponent.Builder FOOD_H_5_0_S_4_0_BUILDER = new FoodComponent.Builder().hunger(5).saturationModifier(0.5f);
-	public static final FoodComponent FOOD_H_5_0_S_4_0 = FOOD_H_5_0_S_4_0_BUILDER.build();
-	//roasted beetroot
-	public static final FoodComponent.Builder FOOD_H_4_0_S_3_0_BUILDER = new FoodComponent.Builder().hunger(4).saturationModifier(0.375f);
-	public static final FoodComponent FOOD_H_4_0_S_3_0 = FOOD_H_4_0_S_3_0_BUILDER.build();
-	//roasted pumpkin
-	public static final FoodComponent.Builder FOOD_H_6_0_S_2_0_BUILDER = new FoodComponent.Builder().hunger(6).saturationModifier(0.25f);
-	public static final FoodComponent FOOD_6_0_S_2_0 = FOOD_H_6_0_S_2_0_BUILDER.build();
-	//fried egg
-	public static final FoodComponent.Builder FOOD_H3_S5_BUILDER = new FoodComponent.Builder().hunger(3).saturationModifier(0.625f);
-	public static final FoodComponent FOOD_H3_S5 = FOOD_H3_S5_BUILDER.build();
 
-	//foods
-	public static final Item ROASTED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_4_0).group(ItemGroup.MISC));
-	public static final Item ROASTED_BEETROOT = new Item(new FabricItemSettings().food(FOOD_H_4_0_S_3_0).group(ItemGroup.MISC));
-	public static final Item ROASTED_PUMPKIN = new Item(new FabricItemSettings().food(FOOD_6_0_S_2_0).group(ItemGroup.MISC));
-	public static final Item FRIED_EGG = new Item(new FabricItemSettings().food(FOOD_H3_S5).group(ItemGroup.MISC));
+	public MoreBakedFoods() {
 
-	//ingredients
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-	@Override
-	public void onInitialize() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-		//foods
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_carrot"), ROASTED_CARROT);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_beetroot"), ROASTED_BEETROOT);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_pumpkin"), ROASTED_PUMPKIN);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fried_egg"), FRIED_EGG);
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::setup);
 
-		//ingredients
+		RegistryHandler.init();
 
-		System.out.println("More Baked Foods Initialized");
+		MinecraftForge.EVENT_BUS.register(this);
+
 	}
 
+	private void setup(final FMLCommonSetupEvent event) {
 
+	}
+	private void doClientStuff(final FMLClientSetupEvent event) {
+
+	}
 
 }
