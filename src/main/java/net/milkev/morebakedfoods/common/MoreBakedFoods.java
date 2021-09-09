5957341556267657cc46fdacf4f5ab45ecc49400
milkev <kmdre@hotmail.com>
2021-09-09 08:24:19 -0600

    Added baked apple
    baked apple texture may be changed
    also changed up gradles
    made the suggested appleskin dependency actually appleskin

--- src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
+++ src/main/java/net/milkev/morebakedfoods/common/MoreBakedFoods.java
@@ -25,12 +25,16 @@ public class MoreBakedFoods implements ModInitializer {
 	//fried egg
 	public static final FoodComponent.Builder FOOD_H3_S5_BUILDER = new FoodComponent.Builder().hunger(3).saturationModifier(0.625f);
 	public static final FoodComponent FOOD_H3_S5 = FOOD_H3_S5_BUILDER.build();
+	//baked apple
+	public static final FoodComponent.Builder FOOD_H5_S5_BUILDER = new FoodComponent.Builder().hunger(5).saturationModifier(0.625f);
+	public static final FoodComponent FOOD_H5_S5 = FOOD_H5_S5_BUILDER.build();
 
 	//foods
-	public static final Item ROASTED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_4_0).group(ItemGroup.MISC));
-	public static final Item ROASTED_BEETROOT = new Item(new FabricItemSettings().food(FOOD_H_4_0_S_3_0).group(ItemGroup.MISC));
-	public static final Item ROASTED_PUMPKIN = new Item(new FabricItemSettings().food(FOOD_6_0_S_2_0).group(ItemGroup.MISC));
-	public static final Item FRIED_EGG = new Item(new FabricItemSettings().food(FOOD_H3_S5).group(ItemGroup.MISC));
+	public static final Item ROASTED_CARROT = new Item(new FabricItemSettings().food(FOOD_H_5_0_S_4_0).group(ItemGroup.FOOD));
+	public static final Item ROASTED_BEETROOT = new Item(new FabricItemSettings().food(FOOD_H_4_0_S_3_0).group(ItemGroup.FOOD));
+	public static final Item ROASTED_PUMPKIN = new Item(new FabricItemSettings().food(FOOD_6_0_S_2_0).group(ItemGroup.FOOD));
+	public static final Item FRIED_EGG = new Item(new FabricItemSettings().food(FOOD_H3_S5).group(ItemGroup.FOOD));
+	public static final Item BAKED_APPLE = new Item(new FabricItemSettings().food(FOOD_H5_S5).group(ItemGroup.FOOD));
 
 	//ingredients
 
@@ -42,6 +46,7 @@ public class MoreBakedFoods implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_beetroot"), ROASTED_BEETROOT);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "roasted_pumpkin"), ROASTED_PUMPKIN);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fried_egg"), FRIED_EGG);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "baked_apple"), BAKED_APPLE);
 
 		//ingredients
 
